68db23375e466bbb2b25bd1c46a645842e283690
尘柒 <109974035+Tenqui-Noir@users.noreply.github.com>
2026-09-02 14:15:14 +0800

    修改版本号至0.14.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
 }
 
-val appVersionName = "0.14.2"
+val appVersionName = "0.14.3"
 
 fun versionCodeFromName(versionName: String): Int {
     val parts = versionName.split(".")
